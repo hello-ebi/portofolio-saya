8de88e67134ad2af78358a2c9e683255055eddbb
Febrian Eka Putra <febrianekaputra.396@gmail.com>
2025-07-25 14:03:03 +0700

    perbaiki masalah di portofolio

--- ebi/src/components/Portofolio.jsx
+++ ebi/src/components/Portofolio.jsx
@@ -1,4 +1,8 @@
 import formGeneratorsertifikat from "../assets/images/generatorSertifikat.png";
+import portalAbsensi from "../assets/images/portalAbsensi.png"
+import ebihengker from "../assets/images/ebihengker.png"
+import { FaLock } from "react-icons/fa";
+import { FaLockOpen } from "react-icons/fa";
 
 function Portfolio() {
   return (
@@ -6,7 +10,7 @@ function Portfolio() {
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-8">Portofolio Saya</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:scale-110 transition duration-300 ease-in-out group">
             <img
               src={formGeneratorsertifikat}
               alt="generator sertifikat"
@@ -19,15 +23,57 @@ function Portfolio() {
                 Cukup share link, peserta isi sendiri, dan sertifikat otomatis dibuat.
               </p>
               <a
-                href="https://generator-sertifikat.vercel.app" // ganti ini dengan link asli kalau ada yaa~
-                className="text-blue-600 hover:underline"
+                href="https://generator-sertifikat.vercel.app"
+                className="text-white rounded-lg hover:bg-yellow-400 hover:text-black w-max block mx-auto text-center py-2 px-10 bg-stone-950"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                View Project
+                Lihat Project Saya
               </a>
             </div>
           </div>
+
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:scale-110 transition duration-300 ease-in-out group">
+            <img
+              src={portalAbsensi}
+              alt="generator sertifikat"
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h3 className="text-xl font-semibold mb-2">Sistem Absensi Kampus</h3>
+              <p className="text-gray-600 mb-4">
+                Aplikasi absensi berbasis MySQL, Express, React, dan Node.js. Dirancang untuk mempermudah proses absensi secara digital.
+              </p>
+              <span
+                href="https://generator-sertifikat.vercel.app"
+                className="text-white rounded-lg w-max block mx-auto text-center py-2 px-7 bg-red-600"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Masih dalam Tahap Pengembangan
+              </span>
+            </div>
+          </div>
+
+          <a href="#contact" className="bg-white relative rounded-lg shadow-lg backdrop:blur-lg z-0 overflow-hidden group hover:scale-110 transition duration-300 ease-in-out" style={{
+            backgroundImage: `url(${ebihengker})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}>
+
+            <div className="absolute inset-0 bg-black/30 backdrop-blur-md z-0 "></div>
+            <div className="relative text-center text-gray-100 items-center justify-center flex flex-col h-full z-10 p-4">
+              <p className="text-2xl font-bold">Mari Bekerja Sama Untuk Membuka Potensi Saya</p>
+              <div className="relative w-[60px] h-[60px] mx-auto">
+                <FaLock className="absolute inset-0 w-full h-full text-5xl transition-opacity duration-300 group-hover:opacity-0" />
+                <FaLockOpen className="absolute inset-0 w-full text-yellow-400 h-full text-5xl opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+              </div>
+
+            </div>
+          </a>
+
+
         </div>
       </div>
     </section>
