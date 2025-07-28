5372499ef83a0df9a249133161af4367531e92a5
Febrian Eka Putra <febrianekaputra.396@gmail.com>
2025-07-28 16:41:30 +0700

    pake persenan

--- ebi/src/components/Navbar.jsx
+++ ebi/src/components/Navbar.jsx
@@ -8,13 +8,13 @@ function Navbar() {
   return (
     <nav className="bg-stone-950 w-full relative text-white h-[8vh] lg:h-[10vh] p-4 sticky top-0 z-20">
       <div className="container mx-auto flex justify-between items-center">
-<div className="z-30 -translate-y-[25%] translate-x-[5%] lg:-translate-y-9 lg:-translate-x-5 top-0 mt-3">
+<div className="z-30 -translate-y-[25%] translate-x-[5%] lg:-translate-y-[40%] lg:-translate-x-[20%] top-0 mt-3">
   <img src={ebilogogif} alt="ebi" className="w-[150px]" />
 </div>
 
 
         <ul className="md:flex flex flex-row items-end md:block absolute -translate-y-[90%] translate-x-[85%] gap-2
-         lg:-translate-y-6 lg:translate-x-5 md:static md:w-auto top-14 md:top-0 transition-all justify-end">
+         lg:-translate-y-[] lg:translate-x-[5%] lg:static  top-14  transition-all justify-end">
           <li>
             <a href="#hero" className="relative inline-block after:block after:h-[2px] p-2 after:bg-yellow-400 after:scale-x-0 after:origin-center hover:after:scale-x-100 after:transition-transform">
               <FaHome className="text-3xl md:hidden" />
