b316cda198ea99fe1e28379b0d10dab372d899d3
Febrian Eka Putra <febrianekaputra.396@gmail.com>
2025-07-28 08:30:08 +0700

    setting responsif about

--- ebi/src/components/About.jsx
+++ ebi/src/components/About.jsx
@@ -45,27 +45,27 @@ function About() {
           className="container relative left-0 ml-10 h-[80vh] w-[70vw] px-4"
         >
           <h1 className="md:text-3xl font-bold text-gray-100 md:text-left md:mb-4">Tentang Saya</h1>
-          <p className="md:text-3xl text-gray-100 md:text-left md:mb-6">
+          <p className="md:text-3xl text-gray-100 md:text-left md:mb-6 mb-1">
             Halo! Saya Febrian Eka Putra, biasa dipanggil ebi. Saya tinggal di Cikarang, Jawa Barat.
             Saat ini saya sedang menempuh studi S1 Teknik Informatika di STMIK Pamitran.
             Meski masih dalam tahap kuliah, saya sangat antusias dan aktif mendalami dunia pemrograman,
             khususnya di bidang Fullstack Web Development, dan saya fokus mengembangkan skill di ekosistem JavaScript.
           </p>
 
-          <div className="text-gray-100 md:mb-1 md:text-xl font-semibold">Hard Skills</div>
-          <div className="flex flex-wrap gap-1 md:gap-4 text-white text-xl md:text-6xl">
+          <div className="text-gray-100 md:mb-1 text-center md:text-xl text-xl font-semibold">Hard Skills</div>
+          <div className="flex flex-wrap gap-1 justify-center md:gap-4 text-white text-xl md:text-6xl mb-1">
             {skillIcons.map((Icon, index) => (
               <Icon key={index} className="hover:text-yellow-400 text-gray-400 transition-all duration-200" />
             ))}
           </div>
-          <div className="text-gray-100 md:mt-4 md:mb-1 md:text-xl font-semibold">Tools</div>
-          <div className="flex flex-wrap gap-1 md:gap-4 text-white text-xl md:text-6xl">
+          <div className="text-gray-100 md:mt-4 md:mb-1 text-xl text-center md:text-xl font-semibold">Tools</div>
+          <div className="flex flex-wrap gap-1 md:gap-4 text-white justify-center text-xl md:text-6xl mb-1">
             {toolsIcons.map((Icon, index) => (
               <Icon key={index} className="hover:text-yellow-400 text-gray-400 transition-all duration-200" />
             ))}
           </div>
-          <div className="text-gray-100 md:mt-4 md:text-xl font-semibold">Soft Skill</div>
-           <div className="flex">
+          <div className="text-gray-100 text-center md:mt-4 md:text-xl text-xl font-semibold">Soft Skill</div>
+           <div className="flex justify-center">
               <img
               src={kucingcuddle}
               alt="kucingcuddle"
